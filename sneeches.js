13aaeef3af48c2c146dcf7f2f20b94d44333f62a
Craig Quincy <craig.quincy@galvanize.com>
2017-02-22 16:32:23 -0700

    solution to code along

--- sneeches.js
+++ sneeches.js
@@ -1,21 +1,37 @@
 // create a method that returns only star bellied sneeches
 const onlyStars = (sneeches) => {
+  return sneeches.filter(sneech => sneech.stars)
 }
 
 // create a function that a creates a new list of sneeches where stars are changed to an integer count instead of a boolean
-const modifyStarCounts = (sneeches) => {
+const modifyStarCounts = sneeches => {
+  // map!
+  return sneeches.map(sneech => {
+    sneech.stars = sneech.stars ? 1 : 0  // ternary operator
+
+    // if (sneech.stars === true) {
+    //   sneech.stars = 1
+    // }
+    // else {
+    //   sneech.stars = 0
+    // }
+
+    return sneech
+  })
 }
 
 
 // create a method that totals the number of stars in all sneeches
 // note: assume modifyStarCounts has been run
 const totalStars = (sneeches) => {
+  return sneeches.reduce((result, sneech) => result + sneech.stars, 0)
 }
 
 // using forEach add one more star add to each sneech (do not return anything)
 // note: assume modifyStarCounts has been run
 
 const addStars = (sneeches) => {
+  sneeches.forEach(sneech => sneech.stars += 1)
 }
 
 module.exports = {
--- test/test.js
+++ test/test.js
@@ -20,16 +20,106 @@ setSneeches = function() {
 describe('Sneech HOFs', function() {
 
   let sneeches
-  beforeEach(function(){
+  beforeEach(function() {
     sneeches = setSneeches()
   })
 
-  it('onlyStars', function() {
+  it('onlyStars filters out sneeches with stars', function() {
+    // zero test case
+    expect(sneechesFunctions.onlyStars([])).to.deep.equals([])
+
+    // one test case
+    expect(sneechesFunctions.onlyStars([sneeches[0]])).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: true
+      }])
+
+    // many test case
+    expect(sneechesFunctions.onlyStars(sneeches)).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: true
+      }])
   })
 
-  it('modifyStarCounts', function() {})
+  it('modifyStarCounts', function() {
+    // zero test
+    expect(sneechesFunctions.modifyStarCounts([])).to.deep.equals([])
 
-  it('addStars', function() {})
+    // 1 test case
+    expect(sneechesFunctions.modifyStarCounts([sneeches[0]])).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: 1
+      }])
 
-  it('totalStars', function() {})
+    // many test case
+    expect(sneechesFunctions.modifyStarCounts(sneeches)).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: 1
+      },
+      {
+        name: 'Sally',
+        stars: 0
+      },
+      {
+        name: 'Suzie',
+        stars: 0
+      }])
+  })
+
+  it('totalStars', function() {
+    sneeches = sneechesFunctions.modifyStarCounts(sneeches)
+
+    // zero test
+    expect(sneechesFunctions.totalStars([])).to.eq(0)
+
+    // 1 test case
+    expect(sneechesFunctions.totalStars([sneeches[0]])).to.eq(1)
+
+    // many test
+    sneeches.push({name: 'Billy', stars: 10})
+    expect(sneechesFunctions.totalStars(sneeches)).to.eq(11)
+  })
+
+  it('addStars', function() {
+    sneeches = sneechesFunctions.modifyStarCounts(sneeches)
+
+    // zero test
+    let sneechesZero = []
+    sneechesFunctions.addStars(sneechesZero)
+    expect(sneechesZero).to.deep.equals([])
+
+    // 1 test case
+    let oneSneech = [{
+      name: 'Bob',
+      stars: 1
+    }]
+    sneechesFunctions.addStars(oneSneech)
+    expect(oneSneech).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: 2
+      }])
+
+    // many test case
+    sneeches = sneechesFunctions.modifyStarCounts(sneeches)
+    sneechesFunctions.addStars(sneeches)
+    expect(sneeches).to.deep.equals(
+      [{
+        name: 'Bob',
+        stars: 2
+      },
+      {
+        name: 'Sally',
+        stars: 1
+      },
+      {
+        name: 'Suzie',
+        stars: 1
+      }])
+  })
+
 })
